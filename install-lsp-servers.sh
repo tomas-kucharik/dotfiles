0faa625b5a58f36cd43160fb8e60ed1e67f8e51a
Tomas Kucharik <tomas.kucharik@telekom.com>
2021-09-04 20:03:31 +0200

    language servers installation updated

--- install-lsp-servers.sh
+++ install-lsp-servers.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
+
+echo "Installing os independent language servers..."
+
+echo "Installing with npm"
 npm i -g bash-language-server
-
 npm i -g dockerfile-language-server-nodejs
-
 npm i -g graphql-language-service-cli
-
 npm i -g vscode-langservers-extracted
-
-npm i -g vscode-langservers-extracted
-
 npm i -g pyright
-
-# # Add the HashiCorp GPG key
-# curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-# # Add the official HashiCorp Linux repository
-# sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-# sudo apt-get update && sudo apt-get install terraform-ls
-
 npm i -g typescript typescript-language-server
-
 npm i -g vim-language-server
-
 npm i -g vls
 
+echo "Installing with yarn"
 yarn global add yaml-language-server
+
+echo "Installing os dependent language servers"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    echo "Installing with brew"
+    brew install terraform-ls
+else
+    # Expecting Ubuntu.
+    echo "Installing with apt"
+    # Add the HashiCorp GPG key
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+    # Add the official HashiCorp Linux repository
+    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+    sudo apt-get update && sudo apt-get install terraform-ls
+fi
+
