f8f86fb44645e2625410ca16662e22575f74c019
Tomas Kucharik <tomas.kucharik@telekom.com>
2021-09-05 18:15:44 +0200

    install-lsp-servers.sh cleanup and colorized

--- install-lsp-servers.sh
+++ install-lsp-servers.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
-echo "Installing os independent language servers..."
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+RESET='\033[0m'
 
-echo "Installing with npm"
-npm i -g bash-language-server
-npm i -g dockerfile-language-server-nodejs
-npm i -g graphql-language-service-cli
-npm i -g vscode-langservers-extracted
-npm i -g pyright
-npm i -g typescript typescript-language-server
-npm i -g vim-language-server
-npm i -g vls
+echo "${YELLOW}Installing os independent language servers...${RESET}"
 
-echo "Installing with yarn"
-yarn global add yaml-language-server
+echo "${YELLOW}Installing with npm${RESET}"
+npm i -g bash-language-server # bash
+npm i -g dockerfile-language-server-nodejs # dockerfile
+npm i -g graphql-language-service-cli # graphql
+npm i -g vscode-langservers-extracted # html, css, js .etc
+npm i -g pyright # python
+npm i -g typescript typescript-language-server # typescript
+npm i -g vim-language-server # viml
+npm i -g vls # vue
 
-echo "Installing os dependent language servers"
+echo "${YELLOW}Installing with yarn${RESET}"
+yarn global add yaml-language-server # yaml
+
+echo "${YELLOW}Installing os dependent language servers${RESET}"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
-    echo "Installing with brew"
-    brew install terraform-ls
+    echo "${YELLOW}Installing with brew${RESET}"
+    brew install terraform-ls # terraform
 else
     # Expecting Ubuntu.
-    echo "Installing with apt"
+    echo "${YELLOW}Installing with apt${RESET}"
     # Add the HashiCorp GPG key
     curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
     # Add the official HashiCorp Linux repository
     sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-    sudo apt-get update && sudo apt-get install terraform-ls
+    sudo apt-get update && sudo apt-get install terraform-ls # terraform
 fi
 
